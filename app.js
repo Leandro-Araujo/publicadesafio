1ba29ebacc06edfe85b7f465e128cdece87965ea
Leandro Silva Araujo <leandro_sy@hotmail.com>
2020-10-04 19:38:25 -0300

    Fazendo o node executar o appjs diretamente

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var http = require('http');
 
 var Mongoose = require('mongoose');
 var banco = require('./model/db');
@@ -28,6 +29,8 @@ app.use('/', indexRouter);
 app.use('/visualizacao', visualizacao);
 app.use('/insercao', insercao);
 
+app.set('port', process.env.PORT || 3000);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -42,6 +45,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 module.exports = app;
